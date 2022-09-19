f04ff07fa747c9db6410a78fcc1b4446a17ca971
Anton <dedulevitchanton@gmail.com>
2022-09-19 21:14:32 +0900

    Задача 10- Выполнено

---
+++ Task_10/Program.cs
@@ -0,0 +1,27 @@
+﻿/* // Задача 10: Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Ответ 1 
+        int num = new Random().Next(100, 1000);
+        int num1 = num % 100 / 10;
+
+        Console.WriteLine(num);
+        Console.Write($"ответ {num1}"); 
+
+       /*  // Ответ 2. 
+        Console.WriteLine("Введите трёхзначное число");
+        string str = Console.ReadLine();
+        Console.WriteLine("Ответ: \t" + str[^2]);    */
+    }
+}
+
+
+
+
+
